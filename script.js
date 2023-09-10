a30da7d0bbe1fe121d8a2203d94826b9aa7292e4
kamalesh0703 <kamaleshponmani0703@gmai.com>
2023-09-11 01:09:03 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const menubutton=document.querySelector('.menu')
+const navitem=document.querySelector('.nav-item')
+const navopen=document.querySelector('.menu-close');
+const navlink=document.querySelectorAll('.nav-link');
+
+
+menubutton.addEventListener('click',()=>{
+    navitem.classList.toggle('showme');
+})
+function linkAction(){
+    navlink.forEach(n=>n.classList.remove('active'))
+    this.classList.add('active')
+    navitem.classList.toggle('showme')
+}
+
+navlink.forEach(n=>n.addEventListener('click',linkAction))
+
+function send(){
+    let email=document.getElementById('email').value;
+    let fristname=document.getElementById('firstname').value;
+    let lastname=document.getElementById('lastname').value;
+    let phone=document.getElementById('telephone').value;
+    let message=document.getElementById('message').value;
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "kamaleshponmani0703@gmail.com",
+        Password : "3C43434C4645384283BC9181C25AF9D8AC56",
+        To : email ,
+        From :"kamaleshponmani0703@gmail.com",
+        Subject : "New client Contact",
+        Body : "Name:"+fristname+" "+lastname+"<br/> Phone:"+phone+"<br/> Message:"+message
+    }).then(
+      message => alert(message)
+    );
+}
