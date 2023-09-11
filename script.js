e3a47fa1baca8aff907dba3236379ef6b19a2afb
kamalesh0703 <kamaleshponmani0703@gmai.com>
2023-09-11 18:14:21 +0530

    hgfyytf

--- script.js
+++ script.js
@@ -32,21 +32,4 @@ function send(){
     }).then(
       message => alert(message)
     );
-    // Email.send({
-    //     Host: "smtp.gmail.com",
-    //     Username: "sender@email_address.com",
-    //     Password: "Enter your password",
-    //     To: 'receiver@email_address.com',
-    //     From: "sender@email_address.com",
-    //     Subject: "Sending Email using javascript",
-    //     Body: "Well that was easy!!",
-    //     Attachments: [
-    //       {
-    //         name: "File_Name_with_Extension",
-    //         path: "Full Path of the file"
-    //       }]
-    //   })
-    //     .then(function (message) {
-    //       alert("Mail has been sent successfully")
-    //     });
 }
